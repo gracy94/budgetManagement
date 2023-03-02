8ddd9eb85836e6b1e1d1d69d86d7878caa3117fe
gracy94 <gracielafaria94@gmail.com>
2023-03-02 13:53:40 -0300

    Validating that the budget is a number greater than zero

--- budgetManagement/src/components/NewBudget.jsx
+++ budgetManagement/src/components/NewBudget.jsx
@@ -1,21 +1,40 @@
+import { useState } from "react"
+import Message from "./Message"
 
 
-const NewBudget = () => {
+const NewBudget = ({budget, setBudget}) => {
+
+    const [message, setMessage] = useState("")
+
+    const handleBudget = (e) => {
+        e.preventDefault()
+        
+        if (!Number(budget) || Number(budget) <0) {
+            setMessage('It is not a valid budget')
+        } else {
+            
+        }
+    }
+
   return (
     <div className="budget-container container shadow">
-        <form className="form">
+        <form className="form" onSubmit={handleBudget}>
             <div className="field">
                 <label>Define Budget</label>
                 <input 
                     type="text" 
                     className="new-budget"
-                    placeholder="Add Budget"   
+                    placeholder="Add Budget"
+                    value={budget}
+                    onChange= { (e) => setBudget(e.target.value)}
                 />             
             </div>
             <input 
                 type="submit" 
                 value="Add"
             />
+
+            {message && <Message type='error'>{message}</Message>}
         </form>
     </div>
   )
