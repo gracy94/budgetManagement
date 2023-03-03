e276a1e9d59ec491d23c6e85e52ea833e0b4487a
gracy94 <gracielafaria94@gmail.com>
2023-03-03 11:27:38 -0300

    styles and functionality added for new expense modal

--- budgetManagement/src/App.jsx
+++ budgetManagement/src/App.jsx
@@ -1,10 +1,51 @@
 import { useState } from 'react'
+import Header from './components/Header'
+import Modal from './components/Modal';
+import NewExpenseIcon from './img/new_expense.svg'
+import SuscriptionsIcon from './img/subscriptions_icon.svg'
 
 function App() {
+  const [budget, setBudget] = useState(0);
+  const [validBudget, setValidBudget] = useState(false);
+
+  const [modal, setModal] = useState(false)
+  const [animateModal, setAnimateModal] = useState(false)
+
+  const handleNewExpense = () => {
+      setModal(true)
+
+      setTimeout(() => {
+        setAnimateModal(true)
+      }, 1000);
+
+  }
 
   return (
-    <h1>Budget Management</h1>
-  )
+    <div>
+      <Header
+        budget={budget}
+        setBudget={setBudget}
+        validBudget={validBudget}
+        setValidBudget={setValidBudget}
+      />
+
+      {validBudget && (
+        <div className="new-expense">
+          <img 
+              src={NewExpenseIcon} 
+              alt="New Expense Icon" 
+              onClick={handleNewExpense}
+          />
+        </div>) 
+      }
+
+      {modal && <Modal 
+                    setModal={setModal}
+                    animateModal={animateModal}
+                    setAnimateModal={setAnimateModal}
+                />}
+    </div>
+  );
 }
 
 export default App
