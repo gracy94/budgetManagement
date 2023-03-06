97f393226794b7b17bd6acc0957bb73e4b46e4b2
gracy94 <gracielafaria94@gmail.com>
2023-03-06 11:42:04 -0300

    dinamic images added to each spense

--- budgetManagement/src/components/Expense.jsx
+++ budgetManagement/src/components/Expense.jsx
@@ -1,5 +1,22 @@
 import { dateFormat } from "../helpers"
 
+import savingsIcon from '../img/savings_icon.svg'
+import foodIcon from '../img/food_icon.svg'
+import homeIcon from '../img/home_icon.svg'
+import spentIcon from '../img/spent_icon.svg'
+import leisureIcon from '../img/leisure_icon.svg'
+import healtIcon from '../img/healt_icon.svg'
+import subscriptionIcon from '../img/subscriptions_icon.svg'
+
+const iconDictionary = {
+    saving: savingsIcon,
+    food: foodIcon,
+    home: homeIcon,
+    spent: spentIcon,
+    leisure: leisureIcon,
+    healt: healtIcon,
+    subscriptions: subscriptionIcon,
+}
 
 const Expense = ({expense}) => {
   const {category, name, quantity, id, date} = expense
@@ -7,6 +24,10 @@ const Expense = ({expense}) => {
    return (
     <div className="expense shadow">
         <div className="content-amount">
+            <img 
+                src={iconDictionary[category]}
+                alt="Icon" 
+            />
             <div className="expense-description">
                 <p className="category">{category}</p>
                 <p className="expense-name">{name}</p>
@@ -14,6 +35,7 @@ const Expense = ({expense}) => {
                     Added on: {''}
                     <span>{dateFormat(date)}</span>
                 </p>
+                
             </div>
         </div>
         <p className="expense-amount">${quantity}</p>
